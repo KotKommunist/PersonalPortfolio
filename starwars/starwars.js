a6a0b90e5c0e0020e1ac0525fc5a0ed32a88fad0
Anton Churilov <antonchurilov@gmail.com>
2021-03-02 10:56:41 -0700

    got film ordering

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,13 +1,21 @@
 import { films } from '../data/films.js'
 
-console.log(films[6])
+let filmList = document.querySelector('#filmList')
 
-let filmOne = document.querySelector('#film1')
-let filmTwo = document.querySelector('#film2')
-let filmThree = document.querySelector('#film3')
-let filmFour = document.querySelector('#film4')
 
-filmOne.textContent = films[0].title
-filmTwo.textContent = films[1].title
-filmThree.textContent = films[2].title
-filmFour.textContent = films[3].title
+for (let i = 0; i < films.length; i++) {
+    console.log(`The loop counter is: ${i} while the film episode id is: ${films[i].episode_id}`)
+   
+    
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+   let filmItem = document.createElement('li')
+   filmItem.textContent = foundFilm.title
+   filmList.appendChild(filmItem)
+
+ }
+
+ function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    return url.charAt(end - 1)
+   
+ }
