3db57a92b6c13a5528cf3ce310b8b4f5fd9f7256
Anton Churilov <antonchurilov@gmail.com>
2021-03-18 11:06:25 -0600

    added link to front page

--- characters/main.js
+++ characters/main.js
@@ -1,4 +1,5 @@
 import { people } from '../data/people.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const mainElement = document.querySelector('#main')
 
@@ -29,6 +30,7 @@ const otherCharacters = people.filter(person => {
         return person
     } //TODO: make sure to also include gender: 'none'
 })
+
 function populateDOM(characters) {
     removeChildren(mainElement)
     characters.forEach((person) => {
@@ -46,21 +48,6 @@ function populateDOM(characters) {
     })
 }
 
-function getLastNumber(url) {
-    let end = url.lastIndexOf('/')
-    let start = end - 2
-    if (url.charAt(start) === '/') {
-        start++
-    }
-    return url.slice(start, end)
-}
-
-function removeChildren(container) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
-
-}
 
 /* while (element.firstChild) {
     element.removeChild(element.firstChild);
--- films/sw-films.js
+++ films/sw-films.js
@@ -1,9 +1,12 @@
 import { films } from '../data/films.js'
+import { getLastNumber } from '../utils/index.js'
+
 
 let filmList = document.querySelector('#filmList')
 
 
 for (let i = 0; i < films.length; i++) {
+
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let posterFig = document.createElement('figure')
     let figImg = document.createElement('img')
@@ -15,10 +18,4 @@ for (let i = 0; i < films.length; i++) {
     posterFig.appendChild(figCaption)
 
     filmList.appendChild(posterFig)
- }
-
- function getLastNumber(url) {
-    let end = url.lastIndexOf('/')
-    return url.charAt(end - 1)
-   
- }
+}
