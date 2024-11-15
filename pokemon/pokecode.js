8ac769a1c43bab5584f6e1f9193b5eb17d073469
KotKommunist <antonchurilov@gmail.com>
2024-11-15 00:34:41 -0700

    cleaned up the code a little

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -221,8 +221,7 @@ function getImageFileName(pokemon) {
   let pokeId
   if (pokemon.id < 10) pokeId = `00${pokemon.id}`
   if (pokemon.id > 9 && pokemon.id < 100) pokeId = `0${pokemon.id}`
-  if (pokemon.id > 99 && pokemon.id < 1000) pokeId = pokemon.id
-  if (pokemon.id > 999 && pokemon.id < 2000) pokeId = pokemon.id
+  if (pokemon.id > 99 && pokemon.id < 2000) pokeId = pokemon.id
   if (pokemon.id === 3000) {
     return `images/pokeball.png`
   }
